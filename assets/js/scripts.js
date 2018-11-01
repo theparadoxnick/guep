31a40f6ede1193d9c88eb47575c655b86f109218
Nick <nicolas@visie.com.br>
2018-11-01 20:30:23 -0300

    Estruturação de arquivos

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+
+ 	var width = $( window ).width();
+    var ismobile = false;
+
+    if ( width < 1024 ) {
+        ismobile = true;
+    }
+
+  // verifica se é aparelho mobile ou não
+
+    if (ismobile) {
+
+      
+    }
+
+});
