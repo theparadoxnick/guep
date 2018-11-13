5cf5a8ccb94b2a6ecdf6ddf583322bee1ae22e39
Nick <nicolas@visie.com.br>
2018-11-12 23:53:02 -0200

    Criação de Steps em Game JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,14 +4,49 @@
 
 jQuery( function() {
 
-    var start = jQuery( '.intro__start' ),
+    var btn_start = jQuery( '.intro__start' ),
+        game = jQuery( '.game' ),
         intro = jQuery( '.intro' );
 
-    start.on( 'click' , function () {
+    btn_start.on( 'click' , function () {
         
-        jQuery(this).parent( intro ).addClass( '-is-disabled' );
-        
+        intro.addClass( '-is-disabled' );
+        game.addClass( '--start' );
         return false;
     });
+
+    var steps = [ 'step1','step2','step3','step4','step5','step6','step7','step8' ],
+        counter = 0,
+        btn_next = jQuery( '.game__next-step' );
+
+    btn_next.on('click', function() {
+        
+        if (counter>7){
+
+            game.removeClass( '--start' ).addClass( '--end' );
+            
+            setTimeout( function() {
+                game.removeClass( '--step8' ).removeClass( '--end' );
+            }, 5000 );
+            
+            counter = 0;
+
+        } else {
+
+            if (counter > 0) {
+                game.removeClass( '--' + steps[counter - 1]);
+                game.addClass( '--' + steps[counter]);
+                counter++;
+            } else {
+                game.addClass( '--' + steps[counter]);
+                counter++;
+            }
+            
+            return false;
+
+        }
+        
+    });
+    
 
 });
